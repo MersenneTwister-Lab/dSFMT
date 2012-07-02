981f0c77ce87830eb1865a6bb13a22e16947b5f5
Mutsuo Saito <saito@math.sci.hiroshima-u.ac.jp>
2012-07-02 14:16:34 +0900

    release sukoshi mae

--- jump/test-jump.cpp
+++ jump/test-jump.cpp
@@ -44,7 +44,7 @@ int main(int argc, char * argv[]) {
 	printf("%s -s|-c [prefix]\n", argv[0]);
 	return -1;
     }
-    string prefix = "lcm.";
+    string prefix = "magic.";
     if (argc >= 3) {
 	prefix = argv[2];
     }
